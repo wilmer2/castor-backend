9a2c4593b6f455060232eb8b4b64a6f09b0b3b64
wilmer2 <mvc_wil@outlook.es>
2016-07-02 18:36:42 -0430

    Create Rooms Table

---
+++ database/migrations/2016_07_02_210222_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_id')->unsigned();
+            $table->string('code_number')->unique();
+            $table->enum('state', ['disponible', 'mantenimiento', 'ocupada', 'fuera de servicio']);
+            
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('rooms');
+    }
+}
