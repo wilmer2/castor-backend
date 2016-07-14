5596e6c06b6934e83db258c09b2d27ecde4405f7
wilmer2 <mvc_wil@outlook.es>
2016-07-13 19:49:47 -0430

    creat settings table

---
+++ database/migrations/2016_07_02_212450_create_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('price_day');
+            $table->float('price_hour');
+            $table->time('time_minimum');
+            $table->boolean('active_impost');
+            $table->float('impost');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('settings');
+    }
+}
