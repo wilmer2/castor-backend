63029e79ee36bbaa6fab492e29759a288474e74a
wilmer2 <mvc_wil@outlook.es>
2016-07-13 23:56:44 -0430

    add sumDay function

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,36 @@
 <?php
 
+use App\Models\Setting;
+use Carbon\Carbon;
 
 function createHour($hour) {
-  return $formatHour = date("H:i", strtotime($hour));
+  return date("H:i", strtotime($hour));
+}
+
+function sumHour($hour, $sumHour) {
+   $time = explode(':', $sumHour);
+   $addHour = date('H:i', strtotime($hour. '+ '. $time[0].' hours'));
+   $totalTime = date('H:i', strtotime($addHour. '+'.$time[1].' minutes'));
+
+   return $totalTime;
+}
+
+function currentHour() {
+  return date('H:i');
+}
+
+function currentDate() {
+  return Carbon::today()->format('Y-m-d');
+}
+
+function addDay($date) {
+  return Carbon::parse($date)->addDay()->format('Y-m-d');
+}
+
+function subDay($date) {
+  return Carbon::parse($date)->subDay()->format('Y-m-d');
+}
+
+function getSetting() {
+  return Setting::find(1);
 }
