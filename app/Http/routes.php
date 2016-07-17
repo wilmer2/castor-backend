de44e75fcd80d1bc34c79089d61c2a61d6ff7f99
wilmer2 <mvc_wil@outlook.es>
2016-07-17 16:50:32 -0430

    Add Route for Update Hour Reservation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,5 +15,6 @@ Route::put('rooms/{roomId}', 'RoomController@update');
 //Route Rental
 Route::post('rentals', 'RentalController@store');
 Route::post('rentals/reservation', 'RentalController@addReservation');
+Route::put('rentals/{rentalId}/reservation_hour', 'RentalController@updateReservationForHour');
 Route::get('rentals/{rentalId}/rooms_date', 'RentalController@getAvailableDateRoom');
 Route::get('rentals/{rentalId}/rooms_hour', 'RentalController@getAvailableHourRoom');
