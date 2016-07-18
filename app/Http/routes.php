a9776a188aca94735b41fa5f0fd1f71a78b0b513
wilmer2 <mvc_wil@outlook.es>
2016-07-17 21:36:17 -0430

    Add Route for getAvailableDateRoom Action

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,5 +16,6 @@ Route::put('rooms/{roomId}', 'RoomController@update');
 Route::post('rentals', 'RentalController@store');
 Route::post('rentals/reservation', 'RentalController@addReservation');
 Route::put('rentals/{rentalId}/reservation_hour', 'RentalController@updateReservationForHour');
+Route::put('rentals/{rentalId}/reservation_date', 'RentalController@updateReservationForDate');
 Route::get('rentals/{rentalId}/rooms_date', 'RentalController@getAvailableDateRoom');
 Route::get('rentals/{rentalId}/rooms_hour', 'RentalController@getAvailableHourRoom');
