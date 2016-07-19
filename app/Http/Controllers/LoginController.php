b92691d3196e5df5bc7c76297fa861b147542e8b
wilmer2 <mvc_wil@outlook.es>
2016-07-18 22:41:04 -0430

    Create Login Controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Auth;
+
+class LoginController extends Controller {
+
+  public function login(Request $request) {
+    $data = $request->only('email', 'password');
+    $authenticate = auth()->attempt(['email' => $data['email'], 'password' => $data['password']]);
+
+    if($authenticate) {
+        $user = currentUser();
+
+        return response()->json($user);
+    } else {
+        return response()->validation_error('Credenciales no son válidas');
+    }
+  }
+
+  public function logout(Request $request) {
+    auth()->logout();
+
+    return response()->json(['message' => 'Session Finalizada']);
+  }
+}
