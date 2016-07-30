f9617eb1774d351f02f1c575d06778df00c7f66b
wilmer2 <mvc_wil@outlook.es>
2016-07-29 20:44:05 -0430

    Add  Route for Action of Rental Controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,12 +19,56 @@ Route::put('rooms/{roomId}', 'RoomController@update');
 
 //Route Rental
 Route::post('rentals', 'RentalController@store');
-Route::get('rentals/{rentalId}/rooms_date', 'RentalController@getAvailableDateRoom');
-Route::get('rentals/{rentalId}/rooms_hour', 'RentalController@getAvailableHourRoom');
-/*Route::put('rentals/{rentalId}/renovate_date', 'RentalController@renovateDate');
-Route::put('rentals/{rentalId}/renovate_hour', 'RentalController@renovateHour');*/
+Route::get('rentals/{rentalId}/rooms_date', 'ReservationController@getAvailableDateRoom');
+Route::get('rentals/{rentalId}/rooms_hour', 'ReservationController@getAvailableHourRoom');
+Route::post('rentals/{rentalId}/room/{roomId}/checkout', 'RentalController@checkoutRoom');
+Route::post('rentals/{rentalId}/checkout', 'RentalController@checkout');
+//Route::post('rentals/{rentalId}/add_room', 'RentalController@addRooms');
+Route::post('rentals/{rentalId}/room/{roomId}/remove', 'RentalController@removeRoom');
 
 //Route Reservation
-Route::post('rentals/reservation', 'RentalController@addReservation');
-Route::put('rentals/{rentalId}/reservation_hour', 'RentalController@updateReservationForHour');
-Route::put('rentals/{rentalId}/reservation_date', 'RentalController@updateReservationForDate');
+Route::post('rentals/reservation', 'ReservationController@addReservation');
+Route::put('rentals/{rentalId}/reservation_hour', 'ReservationController@updateReservationForHour');
+Route::put('rentals/{rentalId}/reservation_date', 'ReservationController@updateReservationForDate');
+
+
+/*use App\Models\Rental;
+
+Route::get('test', function () {
+  $roomsId = [3, 5];
+
+  $rental = Rental::find(1);
+  $date = currentDate();
+  $pivotData = array_fill(0, count($roomsId), ['check_in' => $date]);
+  $syncData = array_combine($roomsId, $pivotData);
+  
+
+  $rental->rooms()->sync($syncData, false);
+
+ 
+
+  /*dd($rooms);
+
+
+  $speakers  = (array) Input::get('speakers'); // related ids
+  $pivotData = array_fill(0, count($speakers), ['is_speaker' => true]);
+  $syncData  = array_combine($speakers, $pivotData);
+
+  $user->roles()->sync($syncData);
+
+});*/
+use Carbon\Carbon;
+
+Route::get('test', function () {
+  $currentDate = currentDate();
+  $date = Carbon::parse($currentDate);
+  $anotherDate = Carbon::parse($currentDate);
+
+
+  $days = $date->diff($anotherDate)->days;
+
+  dd($days);
+});
+
+
+
