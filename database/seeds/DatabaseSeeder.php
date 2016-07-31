c4f378790b1949b6cfae449c55ed1802f1c03dc0
wilmer2 <mvc_wil@outlook.es>
2016-07-31 10:42:29 -0430

    Call TypesTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
       $this->call(RolesTableSeeder::class);
       $this->call(UsersTableSeeder::class);
       $this->call(ClientsTableSeeder::class);
+      $this->call(TypesTableSeeder::class);
     }
     
 }
