5acb3a0385d4b171503edc5ccb2fd02b3897842f
wilmer2 <mvc_wil@outlook.es>
2016-07-31 10:06:49 -0430

    Call Client Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
       $this->call(SettingsTableSeeder::class);
       $this->call(RolesTableSeeder::class);
       $this->call(UsersTableSeeder::class);
+      $this->call(ClientsTableSeeder::class);
     }
     
 }
