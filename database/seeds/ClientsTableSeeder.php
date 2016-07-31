807a79fc5165cd83e2e6de359260b36a8710b67c
wilmer2 <mvc_wil@outlook.es>
2016-07-31 10:05:11 -0430

    Create Client Seeder

---
+++ database/seeds/ClientsTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClientsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+      factory('App\Models\Client', 2)->create();
+    }
+}
