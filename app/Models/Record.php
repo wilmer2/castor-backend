a9cde61f85f55ed367955585a5fe04ff48f86d54
wilmer2 <mvc_wil@outlook.es>
2016-08-06 15:05:21 -0430

    Add to array fillable arrival_date propertie

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use LaravelArdent\Ardent\Ardent;
 
 class Record extends Ardent {
-  protected $fillable = ['rental_id', 'type', 'departure_date', 'departure_time' ,'first'];
+  protected $fillable = [
+    'rental_id', 
+    'type', 
+    'arrival_date', 
+    'departure_date', 
+    'departure_time' ,
+    'first'
+  ];
 
   public function rental() {
     return $this->belongsTo(Rental::class);
