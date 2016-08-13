c57e65fb7b2562d6fc3a9d8d69311bb43e68b09d
wilmer2 <mvc_wil@outlook.es>
2016-08-12 21:54:49 -0430

    Add listeners

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -19,6 +19,7 @@ class EventServiceProvider extends ServiceProvider
 
         'App\Events\RentalWasAssigned' => [
             'App\Listeners\CalculatePayment',
+            'App\Listeners\CheckRentalRooms'
             //'App\Listeners\AssignToMove',
         ],
     ];
