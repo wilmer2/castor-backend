905a963331da4cb6a151f7c95fab6df323e76639
wilmer2 <mvc_wil@outlook.es>
2016-08-12 21:56:48 -0430

    Listener for check state room

---
+++ app/Listeners/CheckRentalRooms.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\RentalWasAssigned;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\Room;
+
+class CheckRentalRooms
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  RentalWasAssigned  $event
+     * @return void
+     */
+    public function handle(RentalWasAssigned $event) {
+        $rental = $event->rental;
+
+        if($rental->checkout) {
+            $state = 'disponible';
+        } else {
+            $state = 'ocupada';
+        }
+
+        $rooms = $rental->getEnabledRooms()
+        ->get();
+
+        foreach ($rooms as $room) {
+            $room->state = $state;
+            $room->save();
+        }
+
+        $rental->stateRoomCheckout();
+        $this->stateRoomWithoutRental();
+    }
+
+
+    public function stateRoomWithoutRental() {
+        $rooms = Room::whereDoesntHave('rentals')
+        ->where('state', 'ocupada');
+
+        if($rooms->count() > 0) {
+            $rooms = $rooms->get();
+
+            foreach ($rooms as $room) {
+               $room->state = 'disponible';
+               $room->save();
+            }
+        }
+    }
+}
