c00aa573a37318067513ce5308c9e0e5c5decb1c
wilmer2 <mvc_wil@outlook.es>
2016-08-14 20:29:19 -0430

    Add move_id property

--- database/migrations/2016_07_24_162808_create_records_table.php
+++ database/migrations/2016_07_24_162808_create_records_table.php
@@ -15,13 +15,18 @@ class CreateRecordsTable extends Migration
         Schema::create('records', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('rental_id')->unsigned();
+            $table->integer('move_id')->unsigned()->nullable();
             $table->string('type');
+            $table->string('payment_type');
             $table->boolean('first');
             $table->date('arrival_date');
             $table->date('departure_date')->nullable();
             $table->time('departure_time');
+            $table->float('amount');
+            $table->float('amount_total');
             
             $table->foreign('rental_id')->references('id')->on('rentals')->onDelete('cascade');
+            $table->foreign('move_id')->references('id')->on('moves');
             $table->timestamps();
         });
     }
