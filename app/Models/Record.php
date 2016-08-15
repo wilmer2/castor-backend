90c420dcb0e519c5d6a1c18f2e4db16ad2272ceb
wilmer2 <mvc_wil@outlook.es>
2016-08-14 20:28:19 -0430

    Add relationship with Move Model

--- app/Models/Record.php
+++ app/Models/Record.php
@@ -7,15 +7,23 @@ use LaravelArdent\Ardent\Ardent;
 class Record extends Ardent {
   protected $fillable = [
     'rental_id', 
+    'move_id',
     'type', 
     'arrival_date', 
     'departure_date', 
     'departure_time' ,
-    'first'
+    'first',
+    'payment_type',
+    'amount',
+    'amount_total'
   ];
 
   public function rental() {
     return $this->belongsTo(Rental::class);
+  }
+
+  public function move() {
+    return $this->belongsTo(Move::class);
   }
 
 }
