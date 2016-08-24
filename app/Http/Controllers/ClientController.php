7388985da5e23b4c564f32f5b6827619ba5f0a71
wilmer2 <mvc_wil@outlook.es>
2016-08-23 21:24:43 -0430

    Show Client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -9,6 +9,12 @@ use App\Models\Client;
 
 class ClientController extends Controller {
 
+  public function index() {
+    $clients = Client::all();
+
+    return response()->json($clients);
+  }
+
   public function store(Request $request) {
     $inputData = $request->all();
 
@@ -19,6 +25,13 @@ class ClientController extends Controller {
     } else {
         return response()->validation_error($newClient->errors());
     }
+  }
+
+  public function show($clientId) {
+    $client = Client::findOrFail($clientId);
+    $client = $client->getData();
+
+    return response()->json($client);
   }
 
   public function update(Request $request, $clientId) {
