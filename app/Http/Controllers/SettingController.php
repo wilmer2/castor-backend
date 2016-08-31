bf461fbe92ebc72bd02605db9bbd0ddc314f0d72
wilmer2 <mvc_wil@outlook.es>
2016-08-30 22:45:16 -0430

    Add Update Action

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -2,6 +2,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use App\Http\Requests;
 use App\Models\Setting;
 
 class SettingController extends Controller {
@@ -10,5 +12,17 @@ class SettingController extends Controller {
     $setting = getSetting();
 
     return response()->json($setting);
+  }
+
+  public function update(Request $request) {
+    $setting = getSetting();
+
+    $inputData = $request->all();
+    
+    if($setting->update($inputData)) {
+        return response()->json($setting);
+    } else {
+        return response()->validation_error($setting->errors());
+    }
   }
 }
