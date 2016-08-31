a71d877a6ba312e6ab522116593eb288ff2c40e7
wilmer2 <mvc_wil@outlook.es>
2016-08-30 22:41:52 -0430

    Delete Action

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -10,6 +10,27 @@ use App\Http\Tasks\RoomTask;
 
 class RoomController extends Controller {
 
+  public function index(Request $request) {
+    $rooms = Room::all();
+
+    return response()->json($rooms);
+  }
+
+  public function getRoomsMaintenance() {
+    $rooms = Room::where('state', 'mantenimiento')
+    ->get();
+
+    return response()->json($rooms);
+  }
+
+  public function getRoomsDisabled() {
+    $rooms = Room::where('state', 'desahabilitada')
+    ->get();
+
+    return response()->json($rooms);
+  }
+
+
   public function store(Request $request) {
     $inputData = $request->all();
 
@@ -57,5 +78,51 @@ class RoomController extends Controller {
     $rooms = $roomTask->getAvailableHourRoom();
 
     return response()->json($rooms);
+  }
+
+  public function availableAddHourRooms(Request $request, RoomTask $roomTask, $startDate, $starTime, $endTime) {
+    $roomTask->setData($startDate, $starTime, null, $endTime);
+
+    $rooms = $roomTask->getAvailableHourRoom();
+
+    return response()->json($rooms);
+  }
+
+  public function disableRoom(Request $request, $roomId) {
+    $room = Room::findOrFail($roomId);
+
+    if($room->hasRental()->count() > 0) {
+        return response()->validation_error('La habitación no se pude desabilitar si tiene hospedaje vigente o esta reservada');
+    }
+
+    $room->state = 'desahabilitada';
+    $room->save();
+
+    return response()->json(['message' => 'La habitacion ha sido desahabilitada']);
+  }
+
+  public function enableRoom(Request $request, $roomId) {
+    $room = Room::findOrFail($roomId);
+
+    if($room->state != 'desahabilitada') {
+        return response()->validation_error('Esta habitación no esta desahabilitada');
+    }
+
+    $room->state = 'disponible';
+    $room->save();
+
+    return response()->json(['message' => 'La habitacion ha sido habilitada']);
+  }
+
+  public function delete(Request $request, $roomId) {
+    $room = Room::findOrFail($roomId);
+
+    if($room->hasRental()->count() > 0) {
+        return response()->validation_error('La habitación no puede ser borrada si tiene hospedaje vigente o esta reservada');
+    }
+
+    $room->delete();
+
+    return response()->json(['message' => 'La  habitacion ha sido borrada']);
   }
 }
