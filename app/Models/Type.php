1a7f534e704160a60734044aab5051434c05708d
wilmer2 <mvc_wil@outlook.es>
2016-08-31 20:33:00 -0430

    Add CountRoom Method

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -6,7 +6,7 @@ use LaravelArdent\Ardent\Ardent;
 
 class Type extends Ardent {
 
-  protected $fillable = ['title', 'description', 'increment'];
+  protected $fillable = ['title', 'description', 'increment', 'img_url'];
 
   public static $rules = [
     'title' => 'required|unique:types,title',
@@ -17,12 +17,23 @@ class Type extends Ardent {
   public static $customMessages = [
     'title.required' => 'El tipo es obligatorio',
     'title.unique' => 'Ya existe este tipo',
-    'description' => 'La descripción es obligatoria',
+    'description.required' => 'La descripción es obligatoria',
     'increment.numeric' => 'El monto extra debe ser un número'
   ];
 
   public function rooms() {
     return $this->hasMany(Room::class);
+  }
+
+  public function beforeSave() {
+    if($this->img_url == null) {
+        $this->img_url = 'http://castor_backend/img/default/default_room.jpg';
+    }
+  }
+
+  public function countRooms() {
+    return $this->rooms()
+    ->count();
   }
 
 }
