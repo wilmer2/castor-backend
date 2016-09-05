50bfd252d0170c8b4087cacd4fd7eedaaf0f4bd2
wilmer2 <mvc_wil@outlook.es>
2016-09-04 21:23:15 -0430

    Add Rule Validaton

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -14,6 +14,24 @@ class Setting extends Ardent {
     'impost'
   ];
 
+  public static $rules = [
+    'price_day' => 'required|numeric',
+    'price_hour' => 'required|numeric',
+    'time_minimum' => 'required|date_format:H:i:s',
+    'impost' => 'numeric'
+  ];
+
+  public static $customMessages = [
+    'price_day.required' => 'El precio por dia es obligatori0',
+    'price_day.numeric' => 'El precio por dia deber ser un número',
+    'price_hour.required' => 'El precio por hora es obligatorio',
+    'price_hour.numeric' => 'El precio por hora debe ser un número',
+    'time_minimum.required' => 'El tiempo minimo es obligatorio',
+    'time_minimum.date_format' => 'El tiempo minimo es un formato inválido',
+    'impost.numeric' => 'El impuesto debe ser un número'
+  ];
+
+
   public function calculateImpost($amount) {
     $impost = 0;
 
