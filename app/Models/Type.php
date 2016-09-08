1ae07dad9d28552862036fc9df437fd8d2c94525
wilmer2 <mvc_wil@outlook.es>
2016-09-07 23:12:44 -0430

    Upload Image

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -6,6 +6,9 @@ use LaravelArdent\Ardent\Ardent;
 
 class Type extends Ardent {
 
+  private $localUrl = 'http://castor_backend';
+  private $defaultImg = '/img/default/default_room.jpg';
+
   protected $fillable = ['title', 'description', 'increment', 'img_url'];
 
   public static $rules = [
@@ -27,13 +30,84 @@ class Type extends Ardent {
 
   public function beforeSave() {
     if($this->img_url == null) {
-        $this->img_url = 'http://castor_backend/img/default/default_room.jpg';
+        $this->img_url = $this->localUrl.$this->defaultImg;
     }
   }
 
-  public function countRooms() {
+  public function countRooms($mime, $img) {
     return $this->rooms()
     ->count();
+  }
+
+  public function uploadImg($img, $mime) {
+    $d = $this->dirExists();
+
+    $dir  = $this->getDirName();
+    $file = $this->getNameFile($mime);
+    $path = $dir.$file;
+    
+    $imgUpload = str_replace('data:image/'.$mime.';base64,', '', $img);
+    $imgUpload = str_replace(' ',  '+', $imgUpload);
+    $data = base64_decode($imgUpload);
+
+    file_put_contents($path, $data);
+
+    $this->img_url = $this->getUrl($path);
+
+    $this->save();
+
+    return $d;
+  }
+
+  public function dirExists() {
+    $dir = $this->getDirName();
+
+    if(!file_exists($dir)) {
+        mkdir($dir, 0777, true);
+    } else {
+        $imgUrlDefault = $this->localUrl.$this->defaultImg;
+
+        if($this->img_url != $imgUrlDefault) {
+            $path = $this->pathFile($this->img_url);
+            $this->imgDrop($path);
+
+            return 'delete';
+        }
+
+        return 'not delete';
+    }
+  }
+
+  public function pathFile($imgUrl) {
+    $dirPublic = public_path();
+    $path = str_replace($this->localUrl, $dirPublic, $imgUrl);
+
+    return $path;
+  }
+
+  public function imgDrop($path) {
+    if(file_exists($path)) {
+         unlink($path);
+    }
+  }
+
+  public function getDirName() {
+    return public_path().'/img/types/type'.$this->id;
+  }
+
+  public function getNameFile($mime) {
+    $image = uniqid('/img', true);
+    $image = str_replace('.', '', $image);
+    $image = $image.$this->id.'.'.$mime;
+
+    return $image;
+  }
+
+  public function getUrl($path) {
+    $dirPublic = public_path();
+    $url = str_replace($dirPublic, $this->localUrl, $path);
+
+    return $url;
   }
 
 }
