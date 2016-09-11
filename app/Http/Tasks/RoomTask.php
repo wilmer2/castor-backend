c2037cfa2b3135a3273e2acbcc32bb7eac46859e
wilmer2 <mvc_wil@outlook.es>
2016-09-11 00:23:30 -0430

    Change Message for Departure Date

--- app/Http/Tasks/RoomTask.php
+++ app/Http/Tasks/RoomTask.php
@@ -174,7 +174,7 @@ class RoomTask {
     if($departureDate != null) {
       if(!$this->rentalValidator->isValidBetweenDates($arrivalDate, $departureDate)) {
           $valid = false;
-          $this->setMessage('La fecha de salida no es acorde a los tiempos');
+          $this->setMessage('La fecha de salida debe ser mayor');
       }
     } 
     
