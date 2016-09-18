afafd13974961b1916b018bf2c29d7aafd0fe98e
wilmer2 <mvc_wil@outlook.es>
2016-09-17 21:07:07 -0430

    Register Security Middleware

---
+++ app/Http/Middleware/SecurityMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class SecurityMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {   
+        if (!auth()->check()) {
+            return response('Unauthorized.', 401);
+        }
+
+        return $next($request);
+    }
+}
